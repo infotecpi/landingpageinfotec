df15e3c322230a7f3e6ff2ba9547be070cb05a5d
Jarod Cavalcante <jarodsim@gmail.com>
2021-08-03 22:28:20 -0300

    chore: alterando titulo do site

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <div className={styles.container} id="topo">
       <Head>
-        <title>INFO-TEC Piauí</title>
+        <title>INFO-TEC - O melhor provedor de internet fibra da região</title>
         <meta charSet="utf-8" />
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
@@ -28,20 +28,20 @@ export default function Home() {
 
         {/* SEO */}
         {/*  <!-- Primary Meta Tags --> */}
-        <meta name="description" content="Info-tec, seu melhor provedor de internet fibra-óptica. Info-tec, mais que produtos, vendemos soluções." />
+        <meta name="description" content="Info-tec, seu melhor provedor de internet fibra-óptica. Mais que produtos, vendemos soluções." />
 
         {/* <!-- Open Graph / Facebook --> */}
         <meta property="og:type" content="website" />
         <meta property="og:url" content="https://infotecpi.com.br/" />
         <meta property="og:title" content="INFO-TEC" />
-        <meta property="og:description" content="Info-tec, seu melhor provedor de internet fibra-óptica. Info-tec, mais que produtos, vendemos soluções." />
+        <meta property="og:description" content="Info-tec, seu melhor provedor de internet fibra-óptica. Mais que produtos, vendemos soluções." />
         <meta property="og:image" content="" />
 
         {/* <!-- Twitter --> */}
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://infotecpi.com.br/" />
         <meta property="twitter:title" content="INFO-TEC" />
-        <meta property="twitter:description" content="Info-tec, seu melhor provedor de internet fibra-óptica. Info-tec, mais que produtos, vendemos soluções." />
+        <meta property="twitter:description" content="Info-tec, seu melhor provedor de internet fibra-óptica. Mais que produtos, vendemos soluções." />
         <meta property="twitter:image" content="" />
       </Head>
       {/* HEADER */}
